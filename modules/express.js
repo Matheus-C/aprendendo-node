8f459799ef2ca8b929f874d54119d5cb843fa2a5
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-08-22 15:11:37 -0300

    operações crud

--- modules/express.js
+++ modules/express.js
@@ -9,16 +9,27 @@ app.get("/home", (req, res) => {
     res.status(200).send('<h1>home Page</h1>');
 });
 
-app.get("/users", (req, res) => {
-    const users = [{
-        name: "john",
-        email: "john@email.com",
-    },
-    {
-        name: "anne",
-        email: "anne@email.com"
-    },];
-    res.status(200).json(users);
+app.get("/users", async (req, res) => {
+    try{
+        const users = await UserModel.find({});
+        res.status(200).json(users);
+    }
+    catch (error) {
+        return res.status(500).send(error.message);
+    }
+    
+});
+
+app.get("/users/:id", async (req, res) => {
+    try{
+        const id = req.params.id;
+        const users = await UserModel.findById(id);
+        res.status(200).json(users);
+    }
+    catch (error) {
+        return res.status(500).send(error.message);
+    }
+    
 });
 
 app.post("/users", async (req, res)=>{
@@ -27,6 +38,34 @@ app.post("/users", async (req, res)=>{
 
         res.status(201).json(user);
         console.log("user criado");
+    }catch(error){
+        res.status(500).send(error.message);
+        console.log(error.message);
+    }
+    
+});
+//atualizar apenas alguns campos, para todos utilizar put
+app.patch("/users/:id", async (req, res)=>{
+    try{
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true});
+
+        res.status(200).json(user);
+        console.log("user modificado");
+    }catch(error){
+        res.status(500).send(error.message);
+        console.log(error.message);
+    }
+    
+});
+
+app.delete("/users/:id", async (req, res)=>{
+    try{
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndDelete(id);
+
+        res.status(200).send("entrada deletada com sucesso");
+        console.log("entrada deletada com sucesso");
     }catch(error){
         res.status(500).send(error.message);
         console.log(error.message);
